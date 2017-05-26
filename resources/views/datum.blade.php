673b1eb11f586ad524cc59a6e564cbac582f1464
JngWny <gunn856@icloud.com>
2017-05-26 17:36:22 -0400

    initial commit

---
+++ resources/views/datum.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+
+            <table class="table table-bordered table-striped" >
+
+                    <thead>
+                    <tr>
+                        <td>Doctor</td>
+                        <td>PatNum</td>
+                        <td>Finacial Class</td>
+                        <td>Chg Code</td>
+                        <td>Description</td>
+                        <td>DRG</td>
+                        <td>Amount</td>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach ($results as $result)
+
+                    <tr>
+                        <td>{{ $result->AdmDr }}</td>
+                        <td>{{ $result->AccountNum }}</td>
+                        <td>{{ $result->FC }}</td>
+                        <td>{{ $result->ChgCode}}</td>
+                        <td>{{ $result->ChgDescription}}</td>
+                        <td>{{ $result->DRG}}</td>
+                        <td>${{  number_format($result->TxnAmt,2) }}</td>
+                    </tr>
+
+                    @endforeach
+                </tbody>
+            </table>
+
+    </div>
+</div>
+</div>
+<div class="col-md-12 col-md-offset-4">
+<?php echo $results->render(); ?>
+</div>
+@endsection
