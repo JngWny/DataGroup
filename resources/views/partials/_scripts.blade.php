9ed5dcd05122bd9324e69aa64811af44e5c192dd
JngWny <gunn856@icloud.com>
2017-07-27 12:07:38 -0400

    changed js

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -1,8 +1,11 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+   <script
+  src="http://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
 <!-- Bootstrap core CSS -->
 
 
